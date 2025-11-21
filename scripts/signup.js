82518ab8db5fab31218bfa9d931331f171db4ad9
Kathy Yeung <kathy@Kathys-MacBook-Air.local>
2025-11-21 16:36:28 -0500

    added dropdown selection for audience/situation via JS, added SCSS styling for select

--- scripts/signup.js
+++ scripts/signup.js
@@ -1,3 +1,41 @@
+const audience = [
+    {id: 1, name: "Student", value: "Student"},
+    {id: 2, name: "Retiree / Soon-to-be Retirees", value: "Retiree / Soon-to-be Retirees"},
+    {id: 3, name: "Homeowner", value: "Homeowner"},
+    {id: 4, name: "Newcomer to Canada", value: "Newcomer to Canada"}
+]
+
+function populateAudience(data) {
+    const audienceWrapper = document.querySelector('[data-audience-container]');
+
+    const label = document.createElement('label');
+    label.setAttribute('for', 'audience');
+    label.textContent = 'Situation';
+    audienceWrapper.appendChild(label);
+
+    const select = document.createElement('select');
+    select.id = 'audience';
+    select.name = 'audience';
+
+    const defaultOption = document.createElement('option');
+    defaultOption.value = "";
+    defaultOption.textContent = "";
+    defaultOption.disabled = true;
+    defaultOption.selected = true;
+    select.appendChild(defaultOption);
+
+    data.forEach(item => {
+        const option = document.createElement('option');
+        option.value = item.value;
+        option.textContent = item.name;
+        select.appendChild(option);
+    });
+
+    audienceWrapper.appendChild(select);
+};
+
+populateAudience(audience);
+
 const apiUrl = "https://industry-sprint-api-365b460ef11e.herokuapp.com/";
 const apiKey = "e9731023-5287-4340-9b98-22aada555c9c";
 
