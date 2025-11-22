a235af4222dfd077667f915568699a3ad6de2677
Kathy Yeung <kathy@Kathys-MacBook-Air.local>
2025-11-22 11:27:42 -0500

    reorganized the input fields to match TD branding and added the dropdown for income and audience

--- scripts/signup.js
+++ scripts/signup.js
@@ -1,43 +1,78 @@
+// import {apiKey} from "./products";
+
 const audience = [
-    {id: 1, name: "Student", value: "Student"},
-    {id: 2, name: "Retiree / Soon-to-be Retirees", value: "Retiree / Soon-to-be Retirees"},
-    {id: 3, name: "Homeowner", value: "Homeowner"},
-    {id: 4, name: "Newcomer to Canada", value: "Newcomer to Canada"}
+    {id: "audience1", name: "Student", value: "Student"},
+    {id: "audience2", name: "Retiree / Soon-to-be Retirees", value: "Retiree / Soon-to-be Retirees"},
+    {id: "audience3", name: "Homeowner", value: "Homeowner"},
+    {id: "audience4", name: "Newcomer to Canada", value: "Newcomer to Canada"}
 ]
 
 function populateAudience(data) {
     const audienceWrapper = document.querySelector('[data-audience-container]');
 
-    const label = document.createElement('label');
-    label.setAttribute('for', 'audience');
-    label.textContent = 'Situation';
-    audienceWrapper.appendChild(label);
-
-    const select = document.createElement('select');
-    select.id = 'audience';
-    select.name = 'audience';
+    const selectElement = document.createElement('select');
+    selectElement.id = 'audience';
+    selectElement.name = 'audience';
+    selectElement.classList.add('signup-form__input'); 
 
     const defaultOption = document.createElement('option');
     defaultOption.value = "";
-    defaultOption.textContent = "";
+    defaultOption.textContent = "Situation *";
     defaultOption.disabled = true;
     defaultOption.selected = true;
-    select.appendChild(defaultOption);
+    selectElement.appendChild(defaultOption);
 
     data.forEach(item => {
-        const option = document.createElement('option');
-        option.value = item.value;
-        option.textContent = item.name;
-        select.appendChild(option);
+        const optionElement = document.createElement('option');
+        optionElement.value = item.value;
+        optionElement.textContent = item.name;
+        selectElement.appendChild(optionElement);
     });
 
-    audienceWrapper.appendChild(select);
+    audienceWrapper.appendChild(selectElement);
 };
 
-populateAudience(audience);
+const income = [
+    {id: "income1", name: "Under $25,000", value: "Under $25,000"},
+    {id: "income2", name: "$25,000 - $49,999", value: "$25,000 - $49,999"},
+    {id: "income3", name: "$50,000 - $74,999", value: "$50,000 - $74,999"},
+    {id: "income4", name: "$75,000 - $99,999", value: "$75,000 - $99,999"},
+    {id: "income5", name: "$100,000 - $149,999", value: "$100,000 - $149,999"},
+    {id: "income6", name: "$150,000 or more", value: "$150,000 or more"},
+    {id: "income7", name: "Unemployed", value: "Unemployed"}
+]
+
+function populateIncome(data) {
+    const incomeWrapper = document.querySelector('[data-income-container]');
+
+    const selectElement = document.createElement('select');
+    selectElement.id = 'income';
+    selectElement.name = 'income';
+    selectElement.classList.add('signup-form__input'); 
+
+    const defaultOption = document.createElement('option');
+    defaultOption.value = "";
+    defaultOption.textContent = "Income *";
+    defaultOption.disabled = true;
+    defaultOption.selected = true;
+    selectElement.appendChild(defaultOption);
+
+    data.forEach(item => {
+        const optionElement = document.createElement('option');
+        optionElement.value = item.value;
+        optionElement.textContent = item.name;
+        selectElement.appendChild(optionElement);
+    });
+
+    incomeWrapper.appendChild(selectElement);
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+    populateAudience(audience);
+    populateIncome(income);
+});
 
 const apiUrl = "https://industry-sprint-api-365b460ef11e.herokuapp.com/";
-import { apiKey } from "../products.js";
 
 const form = document.getElementById("form");
 
